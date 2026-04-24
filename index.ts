5c617e94dd760a89f7d7ae55e60984fe41bbdf14
mcbalaam <mr.max.kh@gmail.com>
2026-04-24 23:55:15 +0700

    test

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 /// <reference path="./prisma-sdk.d.ts" />
 
-PrismaSDK.register('ping', async () => {
-  console.log('pong');
-  return 'pong';
+const REPLY_TEXT = 'Hello World! Это сообщение было отправлено плагином prismaGram Web!';
+
+PrismaSDK.register('beforeMessageSent', async ({ text, chatId }) => {
+  if (text !== '!hello') return { cancel: false };
+  await PrismaSDK.call('messages.send', chatId, REPLY_TEXT);
+  return { cancel: true };
 });
